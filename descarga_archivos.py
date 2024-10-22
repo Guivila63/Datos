7b3944ca40538f0eece1b725d8e1fe2e6b19684c
Guivila63 <GuillermoVidal@outlook.es>
2024-10-22 16:32:10 +0200

    Update descarga_archivos.py

--- descarga_archivos.py
+++ descarga_archivos.py
@@ -1,7 +1,7 @@
 import requests
 
 #Token ESIOS
-esios_token = ''
+esios_token = 'cf07a31b814d3c538a8ffd0175a202761e6457667990908a3e41e15b726c68a1'
 
 #Este ejmplo es para descargar el i90
 address = "https://api.esios.ree.es/archives/34/download?date_type\u003ddatos\u0026end_date\u003d2023-09-16T23%3A59%3A59%2B00%3A00\u0026locale\u003des\u0026start_date\u003d2023-09-16T00%3A00%3A00%2B00%3A00"
